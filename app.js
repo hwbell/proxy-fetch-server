0bf6552006ee276fdb022cd4f031ce55a1ce5a23
Harry <harold.william.bell@gmail.com>
2019-03-31 09:15:06 -0600

    added cors options

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var cors = require('cors');
+// for cors pre-flight requests
+app.options('*', cors());
+app.use(cors()); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
